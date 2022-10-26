5fcdac7ce8bbe30a14fc62cc9e42201b95a5f484
Helson0110 <hanson5977299@gmail.com>
2022-10-26 15:07:37 +0800

    w08-P2: Show tiles layout with css

--- demo/w08/js/demo_67.js
+++ demo/w08/js/demo_67.js
@@ -8,13 +8,8 @@ const navbar = document.querySelector('#nav');
 window.addEventListener('scroll', function () {
 
   if (window.pageYOffset > 100) {
-
     navbar.classList.add('navbar-fixed');
-
   } else {
-
     navbar.classList.remove('navbar-fixed');
-
   }
-
 });
